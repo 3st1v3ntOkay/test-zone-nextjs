2c96f1ad65d4f1b6a537568df64d1c8b5ce84b6c
Esteban Jimenez Valdez <wiwiwixd732@gmail.com>
2023-10-30 15:21:55 -0700

    feat: DateTimeOptions interface created and set to optionsFormat

--- src/utils/timer.ts
+++ src/utils/timer.ts
@@ -1,10 +1,22 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+interface DateTimeOptions {
+  weekday?: "long" | "short" | "narrow"
+  era?: "long" | "short" | "narrow"
+  year?: "numeric" | "2-digit"
+  month?: "numeric" | "2-digit" | "long" | "short" | "narrow"
+  day?: "numeric" | "2-digit"
+  dayPeriod?: "long" | "short" | "narrow"
+  hour?: "numeric" | "2-digit"
+  minute?: "numeric" | "2-digit"
+  second?: "numeric" | "2-digit"
+  fractionalSecondDigits?: 1 | 2 | 3
+  timeZoneName?: "long" | "short" | "shortOffset" | "longOffset" | "shortGeneric" | "longGeneric"
+  formatMatcher?: "basic" | "best fit"
+}
 
 const timer = 'development mode'
-
 const date = new Date();
 
-const optionsFormat = {
+const optionsFormat: DateTimeOptions = {
   weekday: "long",
   month: "long",
   day: "numeric",
